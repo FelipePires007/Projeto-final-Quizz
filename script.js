d347840f2cd191c8b45a78d2d9065d049c40eb21
Sávio Luiz Santos da Silva <2024107290017@aluno.ifro.edu.br>
2024-02-15 18:59:00 -0400

    commit2

--- script.js
+++ script.js
@@ -10,24 +10,25 @@ import perguntas from "./perguntas";
 
 let perguntaAtual = 0;
 let acertos = 0;
-
+ 
 function loadQuestion() {
-    spnQtd.innerHTML = '${perguntaAtual + 1}/${acertos.length}';
-    const item = acertos[perguntaAtual];
-    alternativas.innerHTML = " ";
-    acertos.innerHTML = item.acertos;
+  spnQtd.innerHTML = `${perguntaAtual + 1}/${acertos.length}`;
+  const item = acertos[perguntaAtual];
+  alternativas.innerHTML = "";
+  acertos.innerHTML = item.acertos;
 
-item.alternativas..forEach(alternativas => {
-const div = document.createElementNS("div");
+  item.alternativas.forEach(alternativas => {
+    const div = document.createElementNS("div");
 
-div.innerHTML = '
-<button class="alternativas" data-corrects="${alternativas.correct}">
-  ${alternativas.option}
-  </button>
-  ';
+    div.innerHTML = `
+    <button class="alternativas" data-corrects="${alternativas.correct}">
+      ${alternativas.option}
+    </button>
+   `;
 
   alternativas.appendchild(div);
-});
+  });
+
 }
 
 loadQuestion();
