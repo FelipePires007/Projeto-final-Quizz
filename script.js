0b62e5fc39edd35ad3616baf06bebc6fea2a7cb8
Felipe Pires <felipegamer649@gmail.com>
2024-02-15 16:12:58 -0300

    alterando css e js

--- script.js
+++ script.js
@@ -10,3 +10,24 @@ import perguntas from "./perguntas";
 
 let perguntaAtual = 0;
 let acertos = 0;
+
+function loadQuestion() {
+    spnQtd.innerHTML = '${perguntaAtual + 1}/${acertos.length}';
+    const item = acertos[perguntaAtual];
+    alternativas.innerHTML = " ";
+    acertos.innerHTML = item.acertos;
+
+item.alternativas..forEach(alternativas => {
+const div = document.createElementNS("div");
+
+div.innerHTML = '
+<button class="alternativas" data-corrects="${alternativas.correct}">
+  ${alternativas.option}
+  </button>
+  ';
+
+  alternativas.appendchild(div);
+});
+}
+
+loadQuestion();
