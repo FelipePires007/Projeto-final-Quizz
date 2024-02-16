8e02901b9f4997a6387c06aaa148d5eaac1052f8
Felipe Pires <felipegamer649@gmail.com>
2024-02-16 13:20:59 -0300

    alterando css e js

---
+++ script.js
@@ -0,0 +1,7 @@
+const perguntas = [
+    "Qual é o nome do dono da vila?" ,
+    "Quem é o personagem que sempre diz 'isso, isso, isso!' ?" ,
+    "Qual o nome do professor ?" ,
+    "Qual é a comida favorita do Quico?" ,
+    "O que o chaves diz quando fica nervoso?"
+] ;
